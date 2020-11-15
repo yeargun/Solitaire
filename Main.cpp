593f4128ed0a3ec619d0f8d150ab215d68393f4a
yeargun <44312765+yeargun@users.noreply.github.com>
2020-11-15 16:13:09 +0300

    pilestofundation daki argumanları pointlerayacam
    çünkü fonk calısmasına ragmen degerleri değiştirmiyor

--- Main.cpp
+++ Main.cpp
@@ -4,56 +4,19 @@
 #include<typeinfo>
 #include<cstdlib>
 using namespace std;
-
-
-string hearts[13]={"H01","H02","H03","H04","H05","H06","H07","H08","H09","H10",
-"H11","H12","H13"};
-string diamonds[13]={"D01","D02","D03","D04","D05","D06","D07","D08","D09","D10",
-"D11","D12","D13"};
-string clubs[13]={"C01","C02","C03","C04","C05","C06","C07","C08","C09","C10",
-"C11","C12","C13"};
-string spades[13]={"S01","S02","S03","S04","S05","S06","S07","S08","S09","S10",
-"S11","S12","S13"};
-
-void wasteToFoundation(){
+bool strCompare(string s1, string s2,int compareLength){
+	for(int i=0;i<compareLength;i++){
+		if(s1[i]!=s2[i]){
+			return false;
+		}
+	}
+	return true;
 }
-
-void wasteToPile(){
-	
-}
-class pile{
-	public:
-		string cards[19][2];
-		int topIndex=0;
-		void addToPileDownward(string Card){
-			cards[topIndex][0]=Card;
-			cards[topIndex][1]="0";
-			topIndex++;
-		}
-		void addToPileUpward(string Card){
-			cards[topIndex][0]=Card;
-			cards[topIndex][1]="1";
-			topIndex++;
-		}
-		void addToPileUpwardMultiple(string* Cards){
-			for(int i = 0;i<sizeof(Cards)/sizeof(Cards[0]);i++){
-				addToPileUpward(Cards[i]);
-			}	
-		}
-		
-		int pilesMaxLayerNumber(pile* piles){
-			int max  = 0;
-			for(int i = 0;i<7;i++){
-				if(piles[i].topIndex > max){
-					max=piles[i].topIndex;
-				}
-			}
-			return max;
-		}		
-};
-
-
-int indexOf(string card){
+string hearts[13]={"H01","H02","H03","H04","H05","H06","H07","H08","H09","H10","H11","H12","H13"};
+string diamonds[13]={"D01","D02","D03","D04","D05","D06","D07","D08","D09","D10","D11","D12","D13"};
+string clubs[13]={"C01","C02","C03","C04","C05","C06","C07","C08","C09","C10","C11","C12","C13"};
+string spades[13]={"S01","S02","S03","S04","S05","S06","S07","S08","S09","S10","S11","S12","S13"};
+int indexOf(string card){  //indexOf("H11")=10 
 	if(card[0]=='H'){
 		for(int i=0;i<13;i++){
 			if(hearts[i]==card){
@@ -89,67 +52,131 @@ class stock{
 		string cards[25]={""};
 };
 
-bool strCompare(string s1, string s2,int compareLength){
-	for(int i=0;i<compareLength;i++){
-		if(s1[i]!=s2[i]){
-			return false;
-		}
-	}
-	return true;
+void wasteToFoundation(){
 }
+
+void wasteToPile(){
+	
+}
+class pile{
+	public:
+		string cards[19][2];
+		int topIndex=0;
+		void addToPileDownward(string Card){
+			cards[topIndex][0]=Card;
+			cards[topIndex][1]="0";
+			topIndex++;
+		}
+		void addToPileUpward(string Card){
+			cards[topIndex][0]=Card;
+			cards[topIndex][1]="1";
+			topIndex++;
+		}
+		void addToPileUpwardMultiple(string* Cards){
+			for(int i = 0;i<sizeof(Cards)/sizeof(Cards[0]);i++){
+				addToPileUpward(Cards[i]);
+			}	
+		}
+		void deleteTopmost(){
+			cards[topIndex-1][0]="";
+			topIndex--;
+			cout<<	"ntered the deleetetopmost function tho";
+		}
+		
+		int pilesMaxLayerNumber(pile* piles){
+			int max  = 0;
+			for(int i = 0;i<7;i++){
+				if(piles[i].topIndex > max){
+					max=piles[i].topIndex;
+				}
+			}
+			return max;
+		}
+		
+		void displayPiles(pile* piles){
+			int maxTopIndex=1;
+			for(int j = 0; j<maxTopIndex;j++){
+				for(int i = 0; i<7;i++){
+					if(piles[i].topIndex>maxTopIndex){
+								maxTopIndex=piles[i].topIndex;}
+					if(piles[i].cards[j][0]!=""&& piles[i].cards[j][1]=="1"){ //if card layed upward 
+						cout<<piles[i].cards[j][0]<<"   ";
+					}
+					else if(piles[i].cards[j][0]!=""){
+						cout<<"@@@   ";
+					}
+					else{
+						cout<<"      ";
+					}
+				}
+				cout<<endl;
+			}
+		}		
+};
 
 class foundation{
 	public:
-		string cards[13];
-		int topIndex=1;
-		string moveToFoundation(string card, foundation* foundations){
-		
-			if(card[0]=='H' && indexOf(card)==foundations[0].topIndex){
-				foundations[0].cards[foundations[0].topIndex+1]=card;
-				foundations[0].topIndex++;
-				return "legal";
-			}
-			else if(card[0]=='D' && indexOf(card)==foundations[0].topIndex){
-				foundations[1].cards[foundations[1].topIndex+1]=card;
-				foundations[1].topIndex++;
-				return "legal";
-			}
-			else if(card[0]=='S' && indexOf(card)==foundations[0].topIndex){
-				foundations[2].cards[foundations[2].topIndex+1]=card;
-				foundations[2].topIndex++;
-				return "legal";
-			}
-			else if(card[0]=='C' && indexOf(card)==foundations[0].topIndex){
-				foundations[3].cards[foundations[3].topIndex+1]=card;
-				foundations[3].topIndex++;
-				return "legal";
-			}
+		string cards[4][12];
+		int topIndex0=0;
+		int topIndex1=0;
+		int topIndex2=0;
+		int topIndex3=0;
+		void getPile0(string card, foundation foundations){
+			foundations.cards[0][topIndex0]=card;
+			topIndex0++;
 		}
-		
+		void getPile1(string card, foundation foundations){
+			foundations.cards[1][topIndex1]=card;
+			topIndex1++;
+		}
+		void getPile2(string card, foundation foundations){
+			foundations.cards[2][topIndex2]=card;
+			topIndex2++;
+		}
+		void getPile3(string card, foundation foundations){
+			foundations.cards[3][topIndex3]=card;
+			topIndex3++;
+		}
+		void displayFoundations(){
+			if(topIndex0!=0){
+				cout<<cards[0][topIndex0-1]<<" ";
+			}else{cout<<"___ ";}
+			
+			if(topIndex1!=0){
+				cout<<cards[1][topIndex1-1]<<" ";
+			}else{cout<<"___ ";}
+			
+			if(topIndex2!=0){
+				cout<<cards[2][topIndex2-1]<<" ";
+			}else{cout<<"___ ";}
+			
+			if(topIndex3!=0){
+				cout<<cards[3][topIndex3-1]<<" ";
+			}else{cout<<"___ ";}
+			
+		}
 };
 
-string pileToFoundation(pile pile1, foundation* foundations){
-			/*if(pile1.cards[pile1.topIndex-1][0]=="H" && pile1.topIndex==foundations[0].topIndex){
-				foundations[0].cards[foundations[0].topIndex+1]=pile1.cards[pile1.topIndex-1];
-				foundations[0].topIndex++;
-				return "legal";
+void pileToFoundation(pile pile, foundation foundations){
+			if(pile.cards[pile.topIndex-1][0][0]=='H' && indexOf(pile.cards[pile.topIndex-1][0]) == foundations.topIndex0){
+				foundations.getPile0(pile.cards[pile.topIndex-1][0], foundations);
+				pile.deleteTopmost();
 			}
-			else if(pile1.cards[pile1.topIndex-1][0]=="D" && pile1.topIndex==foundations[0].topIndex){
-				foundations[1].cards[foundations[1].topIndex+1]=pile1.cards[pile1.topIndex-1];
-				foundations[1].topIndex++;
-				return "legal";
+			else if(pile.cards[pile.topIndex-1][0][0] =='D' && indexOf(pile.cards[pile.topIndex-1][0]) == foundations.topIndex1){
+				foundations.getPile1(pile.cards[pile.topIndex-1][0], foundations );
+				pile.deleteTopmost();
 			}
-			else if(pile1.cards[pile1.topIndex-1][0]=="S" && pile1.topIndex==foundations[0].topIndex){
-				foundations[2].cards[foundations[2].topIndex+1]=pile1.cards[pile1.topIndex-1];
-				foundations[2].topIndex++;
-				return "legal";
+			else if(pile.cards[pile.topIndex-1][0][0] =='S' && indexOf(pile.cards[pile.topIndex-1][0]) == foundations.topIndex2){
+				foundations.getPile2(pile.cards[pile.topIndex-1][0], foundations );
+				pile.deleteTopmost();
 			}
-			else if(pile1.cards[pile1.topIndex-1][0]=="C" && pile1.topIndex==foundations[0].topIndex){
-				foundations[3].cards[foundations[3].topIndex+1]=pile1.cards[pile1.topIndex-1];
-				foundations[3].topIndex++;
-				return "legal";
-			}*/
-			return "illegal";
+			else if(pile.cards[pile.topIndex-1][0][0] =='C' && indexOf(pile.cards[pile.topIndex-1][0]) == foundations.topIndex3){
+				foundations.getPile3(pile.cards[pile.topIndex-1][0], foundations );
+				pile.deleteTopmost();
+			}
+			else{
+				cout<<	"not any of them\n";
+			}
 }
 
 class waste{
@@ -214,14 +241,14 @@ void setTable(string* deck, pile* piles,stock stock){
 	}
 }
 
-void displayTable(pile* piles,waste waste,foundation* foundations){
+void displayTable(pile* piles,waste waste,foundation foundations){
 	//cout<<endl<<"@@@ ___ ___ ___         ___ ___ ___ ___"<<endl<<endl;
 	cout<<endl<<"@@@ "; waste.displayWaste();
-	cout<<"        "<<foundations[0].cards[foundations[0].topIndex-1]<<" "<<
-	foundations[1].cards[foundations[1].topIndex-1]<<" "<<
-	foundations[2].cards[foundations[2].topIndex-1]<<" "<<
-	foundations[3].cards[foundations[3].topIndex-1]<<" \n\n";
-	for(int i = 0;i<piles->pilesMaxLayerNumber(piles);i++){
+	cout<<"        ";
+	foundations.displayFoundations();
+	cout<<endl<<endl;
+	piles[0].displayPiles(piles);
+	/*for(int i = 0;i<piles->pilesMaxLayerNumber(piles);i++){
 		for(int j =0;j<7;j++){
 			if(piles[j].cards[i][1]=="1"){
 				cout<<piles[j].cards[i][0]<<"   ";
@@ -234,18 +261,19 @@ void displayTable(pile* piles,waste waste,foundation* foundations){
 			}
 		}
 		cout<<endl;
-	}
+	}*/
+	
 }
 
 int main(){
 	string deck[52];
 	pile piles[7];
-	foundation foundations[4];
+	foundation foundations;
 	stock stock;
 	waste waste;
 	string command;
 	string tempCard;
-	int tempIndex;
+	int tempPileIndex;
 
 	readDeck("deck.txt",deck);
 	setTable(deck, piles, stock);
@@ -258,14 +286,12 @@ int main(){
         cout << "Unable to open file";
         exit(1); // terminate with error
     }
-    foundations[0].cards[0]="H11";
-    foundations[1].cards[0]="D11";
-    foundations[2].cards[0]="S11";
-    foundations[3].cards[0]="C11";
+    
     cout<<indexOf("H11");
 	for( command; getline( commandFile, command ); ){
 
 		cout<<"\n"<<command<<endl;
+		cout<<endl<<"****************************************"<<endl;
 		
     	
     	if(strCompare(command,"open",4)){ //
@@ -278,7 +304,9 @@ int main(){
 		//	foundations.moveToFoundation(piles[stoi(command[24)]].cards[piles[stoi(command[24])].topIndex], foundations)
 		}
 		else if(strCompare(command,"move to foundation p",20)){ //
-			tempIndex=(int)command[24] - 48;
+			tempPileIndex=(int)command[24] - 48;
+			pileToFoundation(piles[tempPileIndex], foundations);
+		
 		//	if(pileToFoundation(piles[tempIndex], foundations)){
 			//	piles[tempIndex].cards[topIndex]="";
 			//	piles[tempIndex].topIndex--;
@@ -299,6 +327,17 @@ int main(){
 		
     	displayTable(piles, waste,foundations);
 	}
+	
+//	cout<<"asdgadga  |"<<piles[0].cards[0][0];
+	
+//	pileToFoundation(piles[6],foundations);
+//	cout<<"asdgadga  |"<<piles[6].cards[6][0];
+		piles[6].deleteTopmost();
+		displayTable(piles,waste,foundations);
+		cout<<piles[6].topIndex;
+		piles[6].deleteTopmost();
+		displayTable(piles,waste,foundations);
+		cout<<piles[6].topIndex;
 	
 	
 	return 0;
