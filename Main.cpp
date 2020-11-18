7e1b822c0517ad0906d5883eb950dae257ab2d2b
yeargun <44312765+yeargun@users.noreply.github.com>
2020-11-18 22:21:56 +0300

    works fine 100%

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,16 @@
 #include <string>
 #include<cstdlib>
 using namespace std;
-bool strCompare(string s1, string s2,int compareLength){
+ofstream output;
+string charArrayToString(char* a, int size) { //
+    int i;
+    string s = "";
+    for (i = 0; i < size; i++) {
+        s = s + a[i];
+    }
+    return s;
+}
+bool strCompare(string s1, string s2,int compareLength){ // returns true if strings are identical
 	if(compareLength>s1.length() || compareLength>s2.length()){return false;}
 	for(int i=0;i<compareLength;i++){
 		if(s1[i]!=s2[i]){
@@ -18,7 +27,7 @@ void readDeck(string path, string deck[52]){
 	deckFile.open(path.c_str());
 	string x;
 	if (!deckFile) {
-        std::cout << "Unable to open file";
+        output << "Unable to open file";
         exit(1); // terminate with error
     }
 
@@ -32,7 +41,7 @@ string hearts[13]={"H01","H02","H03","H04","H05","H06","H07","H08","H09","H10","
 string diamonds[13]={"D01","D02","D03","D04","D05","D06","D07","D08","D09","D10","D11","D12","D13"};
 string clubs[13]={"C01","C02","C03","C04","C05","C06","C07","C08","C09","C10","C11","C12","C13"};
 string spades[13]={"S01","S02","S03","S04","S05","S06","S07","S08","S09","S10","S11","S12","S13"};
-int indexOf(string card){  //indexOf("H11")=10
+int indexOf(string card){  //indexOf("H11")=10 indexOf("C01")=0 ... Returns the card's magnitude 0-12
 	if(card[0]=='H'){
 		for(int i=0;i<13;i++){
 			if(hearts[i]==card){
@@ -73,13 +82,50 @@ bool magnitudeRule(string s1, string s2){//if magnitude s1+1=s2 return true
 	return true;}
 	else return false;
 }
+int stringToInt(string string1){//Converts integers from 0 to 12 else returns -1
+    if(!string1.compare("0"))return 0;
+    if(!string1.compare("1"))return 1;
+    if(!string1.compare("2"))return 2;
+    if(!string1.compare("3"))return 3;
+    if(!string1.compare("4"))return 4;
+    if(!string1.compare("5"))return 5;
+    if(!string1.compare("6"))return 6;
+    if(!string1.compare("7"))return 7;
+    if(!string1.compare("8"))return 8;
+    if(!string1.compare("9"))return 9;
+    if(!string1.compare("10"))return 10;
+    if(!string1.compare("11"))return 11;
+    if(!string1.compare("12"))return 12;
+    return -1;
+
+}
+string stringSplitter(string string1, char parameter, int index){ // splits the string by the given parameter
+//and returns the preffered indexed string from splitted strings
+    char buffer[string1.length()];
+    int splitTime=-1;
+    int bufferIndex=0;
+    for(int i=0;i<string1.length();i++){
+        if(string1[i]!=parameter){
+            buffer[bufferIndex]=string1[i];
+            bufferIndex++;
+        }
+        else{
+            splitTime++;
+            if(splitTime== index){
+                return charArrayToString(buffer,bufferIndex);
+            }
+            bufferIndex=0;
+        }
+    }
+    return charArrayToString(buffer,bufferIndex);
+}
 
 class stock{
     private:
         string cards[24]={""};
         int topIndex=-1;
     public:
-        string getCard(){
+        string getCard(){ //returns the topmost card
             if(topIndex==-1){
                 return "0";
             }
@@ -89,7 +135,7 @@ class stock{
         }
         bool addCard(string card){
             if(topIndex==23){
-                cout<<"stock is full, you cant add more stuff\n";
+                output<<"\nInvalid Move!\n";
                 return false;
             }
             else{
@@ -103,7 +149,7 @@ class stock{
         }
         void deleteTopmostCard(){
             if(topIndex==-1){
-                cout<<"STOCK IS ALREADY EMPTY\n";
+                output<<"\nInvalid Move!\n";
             }
             else{
                 cards[topIndex]="";
@@ -112,28 +158,24 @@ class stock{
         }
         void  displayStock(){
             if(topIndex==-1){
-                cout<<"___ ";
+                output<<"___ ";
             }
             else{
-                cout<<"@@@ ";
-            }
-        }
-        void stockTest(){
-            for(int i=0;i<=topIndex;i++){
-                cout<<"stock["<<i<<"]="<<cards[i]<<endl;
+                output<<"@@@ ";
             }
         }
 };
 
 class foundation{
     private:
-        string cards[4][12]={};
+        string cards[4][13]={};
         int topIndex0=0;
 		int topIndex1=0;
 		int topIndex2=0;
 		int topIndex3=0;
 	public:
-        bool addCard(string card){
+        bool addCard(string card){  //tries to put the given card to any place at foundation area
+                                    //if it succeeds, returns true
             if(card[0]=='H' && (indexOf(card)==topIndex0)){
                     this->cards[0][topIndex0]=card;
                     topIndex0++;
@@ -155,25 +197,25 @@ class foundation{
         }
         void displayFoundations(){
 			if(topIndex0!=0){
-				cout<<cards[0][topIndex0-1]<<" ";
-			}else{cout<<"___ ";}
+				output<<cards[0][topIndex0-1]<<" ";
+			}else{output<<"___ ";}
 
 			if(topIndex1!=0){
-				cout<<cards[1][topIndex1-1]<<" ";
-			}else{cout<<"___ ";}
+				output<<cards[1][topIndex1-1]<<" ";
+			}else{output<<"___ ";}
 
 			if(topIndex2!=0){
-				cout<<cards[2][topIndex2-1]<<" ";
-			}else{cout<<"___ ";}
+				output<<cards[2][topIndex2-1]<<" ";
+			}else{output<<"___ ";}
 
 			if(topIndex3!=0){
-				cout<<cards[3][topIndex3-1]<<" ";
-			}else{cout<<"___ ";}
+				output<<cards[3][topIndex3-1]<<" ";
+			}else{output<<"___ ";}
 
-			cout<<endl<<endl;
+			output<<endl<<endl;
 		}
 
-		string getCard(int index){
+		string getCard(int index){ //get topmost card from the given indexed foundation
 		    switch(index){
                 case 0:
                     if(topIndex0>0)
@@ -209,24 +251,39 @@ class foundation{
                     break;
             }
 		}
+		bool isFull(){
+            if(topIndex0==13 && topIndex1==13 && topIndex2==13 && topIndex3==13){
+                return true;
+            }
+            return false;
+		}
 };
 
 class pile{
+    /* ---Pile1---
+  0  [card1][downward]
+  1  [card2][downward]
+  2  [card3][downward]
+  3  [card4][upward]
+to show whether the card is laying upward or downward,
+it stores one more string:
+    if its "1" that means the card is laying upward
+    if its "0" that means the card is laying upward
+    cards[0][0] = card1, cards[0][1] = "0"
+    cards[3][0] = card4, cards[0][1] = "1"
+    */
     private:
         string cards[19][2];
         int topIndex=-1;
-
     public:
-        void setPiles(){
-        }
-        void addToPileDownward(string card){ // cards[x][1]="0" represents its an downward card, cards[x][1]="1" represents upward cards
+        void addToPileDownward(string card){
             if(topIndex<18){
                 cards[topIndex+1][0]=card;
                 cards[topIndex+1][1]="0";
                 topIndex++;
             }
             else{
-                cout<<"pile is already full, cant add more\n";
+                output<<"\nInvalid Move!\n";
             }
 		}
 		bool addToPileUpward(string card){
@@ -238,30 +295,21 @@ class pile{
 			}
 			return false;
 		}
-		/*void addToPileUpwardMultiple(string* cards1){
-		    int cardAmount=sizeof(cards1)/sizeof(cards1[0]);
-			if(topIndex+cardAmount<18){
-                for(int i = 0;i<cardAmount;i++){
-                    addToPileUpward(cards1[i]);
-                }
-			}
-		}*/
-		void to(int cardAmount, pile* pile2){
+		void to(int cardAmount, pile* pile2){// transfers card/cards to given pile as parameter
+                                            //from the pile that function has called
 		    pile* pile1= this;
 		    cardAmount++;
             int startingIndex=pile1->topIndex-(cardAmount);
-            string cardsToTransfer[cardAmount];
-
-
-
-
-            //if colors and magnitudes match for transfer
-            //or king goes to an empty pile
-            if ((colorRule(pile1->cards[topIndex+1-cardAmount][0],pile2->getTopmostCard()) &&
-                magnitudeRule(pile1->cards[topIndex+1-cardAmount][0],pile2->getTopmostCard()))
+            //P=if the first card from selected card/cards is laying upward
+            //Q=colors and magnitudes match for transfer
+            //R=if king goes to an empty pile
+            // P && (Q || R)
+            if (((colorRule(pile1->cards[topIndex+1-cardAmount][0],pile2->getTopmostCard())&&
+                  magnitudeRule(pile1->cards[topIndex+1-cardAmount][0],pile2->getTopmostCard()))
                                                 ||
-                ((indexOf(pile1->cards[topIndex+1-cardAmount][0])==12) &&
-                    (pile2->topIndex==-1))){
+        ((indexOf(pile1->cards[topIndex+1-cardAmount][0])==12) &&(pile2->topIndex==-1)))
+                                                &&
+                        pile1->cards[topIndex+1-cardAmount][1]=="1"){
                 for(int i=0;i<cardAmount;i++){
                     pile2->addToPileUpward(pile1->cards[topIndex+1-cardAmount+i][0]);
                 }
@@ -270,49 +318,43 @@ class pile{
                 }
 
             }
-            else{cout<<"invalid\n";}
+            else{output<<"\nInvalid Move!\n";}
 
 		}
 		int getTopIndex(){
             return this->topIndex;
         }
-        int pilesMaxLayerNumber(pile* piles){
-			int max  = 0;
-			for(int i = 0;i<7;i++){
-				if(piles[i].topIndex > max){
-					max=piles[i].topIndex;
-				}
-			}
-			return max;
-		}
         void openPile(){
 			if(this->topIndex==-1){
-				cout<<"Error, you cant open anything!";
+				output<<"\nInvalid Move!\n";
 			}
 			else if(this->cards[this->topIndex][1]=="1"){
-				cout<<"Error, you cant open anything!";
+				output<<"\nInvalid Move!\n";
 			}
 			else{
 				this->cards[this->topIndex][1]="1";
 			}
 		}
 		void displayPiles(pile* piles){
-			int maxTopIndex=1;
+			int maxTopIndex=0;
+			for(int i = 0; i<7;i++){
+                if(piles[i].topIndex>maxTopIndex){
+                        maxTopIndex=piles[i].topIndex;
+                }
+			}
 			for(int j = 0; j<=maxTopIndex;j++){
 				for(int i = 0; i<7;i++){
-					if(piles[i].topIndex>maxTopIndex){
-								maxTopIndex=piles[i].topIndex;}
 					if(piles[i].cards[j][0]!=""&& piles[i].cards[j][1]=="1"){ //if card layed upward
-						cout<<piles[i].cards[j][0]<<"   ";
+						output<<piles[i].cards[j][0]<<"   ";
 					}
 					else if(piles[i].cards[j][0]!=""){
-						cout<<"@@@   ";
+						output<<"@@@   ";
 					}
 					else{
-						cout<<"      ";
+						output<<"      ";
 					}
 				}
-				cout<<endl;
+                    output<<endl;
 			}
 		}
         void deleteTopmostCard(){
@@ -322,10 +364,10 @@ class pile{
         string getTopmostCard(){
             return this->cards[this->topIndex][0];
         }
-		void pileToFoundation(foundation* foundation){
+		void pileToFoundation(foundation* foundation){ //transfers the card from pile to foundation
             if(foundation->addCard(this->getTopmostCard())){
                this->deleteTopmostCard();
-            }else{cout<< "invalid\n";}
+            }else{output<< "\nInvalid Move!\n";}
 		}
 };
 
@@ -338,7 +380,7 @@ class waste{
     public:
         string getCard(){
             if(topIndex==-1){
-                cout<<"you cant get card from empty waste\n";
+                output<<"\nInvalid Move!\n";
                 return "0";
             }
             else{
@@ -356,37 +398,23 @@ class waste{
                 cards[topIndex]="";
                 topIndex--;
             }
+        }
+        void getFromHistoryToWaste(){
+            //gets the old-topmost indexed-non used waste card from history
             if(topIndex==-1 && this->wasteHistoryTopIndex!=-1){
                 waste* waste1 = this;
                 waste1->addCard(getWasteHistoryCard());
                 deleteTopmostWasteHistoryCard();
             }
         }
-        void deleteTopmostCard(int x){ // use this function to save waste to history
-            if(topIndex!=-1){
-                cards[topIndex]="";
-                topIndex--;
-            }
-
-
-        }
-
         void wasteToHistory(){
             int cardAmountInWaste=this->topIndex;
             for(int i = 0;i<=cardAmountInWaste;i++){
-                cout<<"function wasteToHistory "<<i <<"th loop, i add this to history: "<<this->cards[i]<<endl;
                 this->addWasteHistoryCard(this->cards[i]);
             }
             for(int i = 0;i<=cardAmountInWaste;i++){
-                this->deleteTopmostCard(1);
+                this->deleteTopmostCard();
             }
-
-            /*for(int i=0;i<this->topIndex;i++){
-                this->addWasteHistoryCard(this->cards[i]);
-            }
-            for(int j=topIndex;j>0;j--){
-                this->deleteTopmostCard(1);
-            }*/
         }
         string getWasteHistoryCard(){
             if(wasteHistoryTopIndex==-1){
@@ -410,17 +438,21 @@ class waste{
         void displayWaste(){
 			for(int i = 0;i<3;i++){
 				if(cards[i]==""){
-					cout<<"___ ";
+					output<<"___ ";
 				}
 				else{
-					cout<< cards[i]<<" ";
+					output<< cards[i]<<" ";
 				}
 			}
-			cout<<"        ";
+			output<<"        ";
 		}
 		void wasteToFoundation(foundation* foundation1){
             if(foundation1->addCard(getCard()) == true){
                 this->deleteTopmostCard();
+                this->getFromHistoryToWaste();
+            }
+            else{
+                output<<"\nInvalid Move!\n";
             }
 		}
 
@@ -428,7 +460,6 @@ class waste{
 		    int i=0;
 		    while(this->wasteHistoryTopIndex!=-1){
                 stock->addCard(this->getWasteHistoryCard());
-                //cout<<"stock["<<i<<"] = "<<this->getWasteHistoryCard()<<endl;
                 i++;
                 this->deleteTopmostWasteHistoryCard();
 		    }
@@ -442,9 +473,10 @@ class waste{
                             (pile1->getTopIndex()==-1))){
                 pile1->addToPileUpward(this->getCard());
                 this->deleteTopmostCard();
+                this->getFromHistoryToWaste();
             }
             else{
-                cout<<"INVALIDDDDDD\n";
+                output<<"\nInvalid Move!\n";
             }
 		}
 
@@ -456,7 +488,7 @@ class waste{
 		    else{
                 for(int i=0 ; i<=2 && stock->getTopIndex()>-1 ;i++){
                     this->addCard(stock->getCard());
-                    cout<<"loop's i="<<i<<"  i add this to waste "<<stock->getCard()<<endl;
+                   // cout<<"loop's i="<<i<<"  i add this to waste "<<stock->getCard()<<endl;
                     stock->deleteTopmostCard();
                 }
 		    }
@@ -496,7 +528,7 @@ void displayTable(pile* piles,waste waste,foundation foundations,stock stock){
     piles[0].displayPiles(piles);
 }
 
-int main(){
+int main(char* argv[]){
     string deck[52];
 	pile piles[7];
 	foundation foundations;
@@ -504,76 +536,75 @@ int main(){
 	waste waste;
 	string command;
 	string tempCard;
-	int tempIndex,tempIndex2,tempIndex3;
-
-	readDeck("deck.txt",deck);
-	setTable(deck, piles, &stock);
-    cout<<endl;
-	displayTable(piles, waste,foundations,stock);
 	ifstream commandFile;
+	int tempIndex,tempIndex2,tempIndex3;
+	int isntValidIndex=-1;
+	readDeck("deck.txt",deck);
 	commandFile.open("commands.txt");
+	output.open("output.txt");
+	setTable(deck, piles, &stock);
+    output<<endl;
+	displayTable(piles, waste,foundations,stock);
+
     if (!commandFile) {
-        cout << "Unable to open file";
         exit(1); // terminate with error
     }
 
 	for( command; getline( commandFile, command ); ){
-        cout<<"\n"<<command<<endl;
-		cout<<endl<<"****************************************"<<endl<<endl;
+        output<<"\n"<<command<<endl;
 
-        stock.stockTest();
+
     	if(strCompare(command,"open f",6)){ //
             waste.openFromStock(&stock);
 		}
     	else if(strCompare(command,"open",4)){
-			tempIndex=(int)command[5] - 48;
-			piles[tempIndex].openPile();
+			if(stringToInt(stringSplitter(command, ' ', 1)) == isntValidIndex){
+                output<<"Invalid Move!\n";
+			}else{
+                tempIndex=stringToInt(stringSplitter(command, ' ', 1));
+                piles[tempIndex].openPile();
+			}
 		}
 		else if(strCompare(command,"move p",6)){
-    		tempIndex=(int)command[10] - 48;
-    		tempIndex2=(int)command[12] - 48;
-    		tempIndex3=(int)command[14] - 48;
-    		piles[tempIndex].to(tempIndex2, &piles[tempIndex3]);
+            if((stringToInt(stringSplitter(command, ' ', 2)) == isntValidIndex)
+            || (stringToInt(stringSplitter(command, ' ', 3)) == isntValidIndex)){
+                output<<"\nInvalid Move!\n";
+			}else{
+			    tempIndex=stringToInt(stringSplitter(command, ' ', 2));
+                tempIndex2=stringToInt(stringSplitter(command, ' ', 3));
+                tempIndex3=stringToInt(stringSplitter(command, ' ', 4));
+                piles[tempIndex].to(tempIndex2, &piles[tempIndex3]);
+			}
 		}
 		else if(strCompare(command,"move to foundation w",20)){ //
 			waste.wasteToFoundation(&foundations);
 		}
 		else if(strCompare(command,"move to foundation p",20)){ //
-			tempIndex=(int)command[24] - 48;
-			piles[tempIndex].pileToFoundation(&foundations);
+		    if(stringToInt(stringSplitter(command, ' ', 4)) <7){
+                tempIndex=stringToInt(stringSplitter(command, ' ', 4));
+                piles[tempIndex].pileToFoundation(&foundations);
+            }
 		}
 		else if(strCompare(command,"move w",6)){ //
-			tempIndex= (int)command[11] - 48;
-			waste.wasteToPile(&piles[tempIndex]);
+            if(stringToInt(stringSplitter(command, ' ', 2))<7){
+                tempIndex=stringToInt(stringSplitter(command, ' ', 2));
+                waste.wasteToPile(&piles[tempIndex]);
+            }
 		}
-
 		else if(strCompare(command,"exit",4)){ //
-			cout<<"I GOEXIT YES";
+			output<<"\n****************************************\n\n";
+			output<<"Game Over!\n";
+			output.close();
+			exit(0);
 		}
-
+		output<<endl<<"****************************************"<<endl<<endl;
 		displayTable(piles, waste,foundations,stock);
- //   	displayInfos(stock,waste);
-
-
-
 	}
-
-
-
-
+	if(foundations.isFull()){
+        output<<"\n****************************************"<<endl<<endl;
+        output<<"You Win!\n\nGame Over!\n";
+        }
+    output.close();
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
